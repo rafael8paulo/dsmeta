a5a06cff625f01756400a6b1a7de37f3cfd47f60
Rafael Paulo <rafael8paulo@gmail.com>
2022-07-16 17:10:07 -0300

    Axios, useEffect first request

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -2,7 +2,8 @@ import NotificationButton from "../NotificationButton";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "./style.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function SalesCard() {
 
@@ -11,6 +12,12 @@ function SalesCard() {
 
   const [minDate, setMinDate] = useState(min);
   const [maxDate, setMaxDate] = useState(max);
+
+  useEffect(() => {
+    axios.get("https://dsmeta-rpx.herokuapp.com/sales").then(response => {
+      console.log(response.data.content);
+    })
+  }, []);
 
   return (
     <>
